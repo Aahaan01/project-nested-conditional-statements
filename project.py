404aa79af3886c47b61bf4c12b3d7a0235abd390
Aahaan01 <gargaahaan01@gmail.com>
2024-11-02 11:01:50 +0530

    project

---
+++ project.py
@@ -0,0 +1,7 @@
+age=int(input("Please enter your age: "))
+if age>=20:
+    print("You cannot enroll in this class.")
+elif age<=10:
+    print("You cannot enter in this class.")
+else:
+    print("You are allowed to enter the class.")
